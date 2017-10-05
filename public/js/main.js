5d880580671786d42b4c0b27ce185a28454c4aa5
Augusto Inacio <augustoinacio@gmail.com>
2017-10-04 23:11:21 -0300

    Novas inclusoes - Implementa rotas.

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,11 @@
-angular.module('alurapic',['minhasDiretivas']);
+angular.module('alurapic',['minhasDiretivas','ngAnimate','ngRoute'])
+.config(function($routeProvider){
+    $routeProvider.when('/fotos', {
+        templateUrl: 'partials/principal.html',
+        controller: 'FotosController'
+    });
+    $routeProvider.when('/fotos/new', {
+        templateUrl: './partials/foto.html'
+    });
+    $routeProvider.otherwise({redirectTo: '/fotos'});
+});
