2a04ca847467e38e986962301a680e7db4872f0e
Augusto Inacio <augustoinacio@gmail.com>
2017-10-09 20:15:32 -0300

    Inclui diretivas. Funcionalidades Editar/Remover

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,6 +1,6 @@
 angular.module('alurapic').controller('FotosController', function ($scope, $http) {
-    
-    $scope.foto = [];
+
+    $scope.foto = {};
     $scope.filtro = '';
 
     $http.get('v1/fotos')
@@ -11,6 +11,19 @@ angular.module('alurapic').controller('FotosController', function ($scope, $http
             console.log(erro);
         })
 
+    $scope.remover = function (foto) {
+        $http.delete('v1/fotos/' + foto._id)
+            .success(function () {
+                var indiceFoto = $scope.fotos.indexOf(foto);
+                $scope.fotos.splice(indiceFoto, 1);
+                console.log("Foto " + foto.titulo + " removida com sucesso.");
+
+            })
+            .error(function (err) {
+                console.log(err);
+                console.log("Não foi possivel remover a foto " + foto.titulo);
+            })
+    };
     /*var promise = $http.get('v1/fotos');
 
     promise.then(function(retorno){
@@ -18,6 +31,6 @@ angular.module('alurapic').controller('FotosController', function ($scope, $http
     }).catch(function(error){
         console.log(error);
     })*/
-    
+
 
 });
